1f327f79a54a2a4926ded4f4cc1228fb994a0e00
이성웅 <dev.lsw91@gmail.com>
2021-03-17 20:11:25 +0900

    <머스테치로 화면 구성 - 삭제 기능> -뷰,컨트롤러,js,서비스

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -47,4 +47,10 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,11 +1,16 @@
 var main = {
     init : function () {
-        var _this = this;
+        var index = this;
         $('#btn-save').on('click', function () {
-            _this.save();
+            index.save();
         });
+
         $('#btn-update').on('click', function () {
-            _this.update();
+            index.update();
+        });
+
+        $('#btn-delete').on('click', function () {
+            index.delete();
         });
     },
     save : function () {
@@ -44,6 +49,21 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
